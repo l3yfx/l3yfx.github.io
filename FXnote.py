bd9b4fd51d42ae27e08a77c6042e42e60635b62f
ivanlin <ivanlin@MBP.local>
2020-12-24 00:51:44 +0800

    20201223-c

--- FXnote.py
+++ FXnote.py
@@ -11,42 +11,48 @@ def l3yfx(self):
 
 	//Technical
 		def Foundation of 3D
-			\Computer science
-			3D Computer graphic/theory 
-			CG field Mathmatics/Physics
-			Problem solving
-			3D tech 
-				Modeling 
-				Dynamics
-				Texturing
-				Lighting
-				Rendering
-				Compositing
+            Nature
+                Math/Physics
+            Computer science
+                3D Computer graphic/theory
+                    CG field
+                        Mathmatics/Physics
+                        algorithm
+                        Problem solving                        
+                    3D tech
+                        Modeling
+                        Dynamics
+                        Texturing
+                        Lighting
+                        Rendering
+                        Compositing
 		def Realization of 3D 			
-			\3D software/framwork/language 
+			3D software/framwork/language 
 			CG industry Pipeline
 			Workflow(Using Houdini/Production efficieny)
-				Expression
-				Wrangle
-				OP
-				Python
-			\FX concepts - Harness the power of houdini -> Realize nature/imagination in a efficient way.
-				3D data manipulation 
-					data(DataStructure) 
-						Custom attributes 
-						Point,vertex,primitive,(polygon),mesh,normal,UVs 
+
+			FX concepts - Harness the power of houdini -> Realize nature/imagination in a efficient way.
+				3D data flow 
+					Data structure						
+						Point,vertex,primitive,mesh,normal,UVs,primuv
 						Volume 
 							Scalar field(density) 
 							Sign distance(SDF)
 							Vector field
 						PointCloud 
-					data manipulation
-						2 Dimensions
+					Data manipulation
+						Manipulate Reserved/custom attributes in 2 Dimensions
 							Proceduralism CRUD 3D data 
-							Simulation Dynamics 3D data 
-					3D FX Field/catergories
-						Particles/Pyro/fluid/grains/crowds/finiteElementscloth/HairFur/vellum Oceans/wavesRipple
-						destruction wires/rigidBody/ Growth/peeling/disintergration 
+							Simulation Dynamics 3D data					
+						Functions
+							Low_lev
+							High_lev
+					Data Organzation
+						Proceduralism
+					# Group,channel,ramp
+					# 3D FX Field/catergories
+					# 	Particles/Pyro/fluid/grains/crowds/finiteElementscloth/HairFur/vellum Oceans/wavesRipple
+					# 	destruction wires/rigidBody/ Growth/peeling/disintergration 
 					Tool 
 						Vop/Vex
 						AttributesTransfer 
@@ -56,11 +62,15 @@ def l3yfx(self):
 						Python
 						Expression
 				Algorithmic design 
-					Sorting, Grouping
-					Iterative, fractual 
+					# loop,group,offset
+					Math
+						Sorting, Grouping, Iterative, Fractual, Noise
+					Performance optimization
+
 				Lighting concepts 
 				Shading concepts
 				Rendering concepts
+				Compositing concepts
 		def R&D_FX 
 			FX R&D(Practice) FX() + Flow() + Score() 
 			FX() = Basic_idea + Advance_development 
