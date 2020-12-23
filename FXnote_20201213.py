cb84bd53d52587724018e20bdb2d5be00d62412b
ivanlin <ivanlin@MBP.local>
2020-12-24 00:43:10 +0800

    20201223-c

---
+++ FXnote_20201213.py
@@ -0,0 +1,78 @@
+def l3yfx(self):
+	3D = Modeling, animation, texture, lighting, rendering 	
+	FX = 3D.Data_manipulation 
+	VFX = FX() + Composition() 
+	
+	-Essence, process, innerbloom 
+		Essence 
+			Nature inspired 
+		Process
+			Uterlize Computer science to create 3D visual.
+
+	//Technical
+		def Foundation of 3D
+            Nature
+                Math/Physics
+            Computer science
+                3D Computer graphic/theory
+                    CG field
+                        Mathmatics/Physics
+                        algorithm
+                        Problem solving                        
+                    3D tech
+                        Modeling
+                        Dynamics
+                        Texturing
+                        Lighting
+                        Rendering
+                        Compositing
+		def Realization of 3D 			
+			3D software/framwork/language 
+			CG industry Pipeline
+			Workflow(Using Houdini/Production efficieny)
+
+			FX concepts - Harness the power of houdini -> Realize nature/imagination in a efficient way.
+				3D data flow 
+					Data structure						
+						Point,vertex,primitive,mesh,normal,UVs,primuv
+						Volume 
+							Scalar field(density) 
+							Sign distance(SDF)
+							Vector field
+						PointCloud 
+					Data manipulation
+						Manipulate Reserved/custom attributes in 2 Dimensions
+							Proceduralism CRUD 3D data 
+							Simulation Dynamics 3D data					
+						Functions
+							Low_lev
+							High_lev
+					Data Organzation
+						Proceduralism
+					# Group,channel,ramp
+					# 3D FX Field/catergories
+					# 	Particles/Pyro/fluid/grains/crowds/finiteElementscloth/HairFur/vellum Oceans/wavesRipple
+					# 	destruction wires/rigidBody/ Growth/peeling/disintergration 
+					Tool 
+						Vop/Vex
+						AttributesTransfer 
+						solver
+						keyframe
+						Sidefx designed Node
+						Python
+						Expression
+				Algorithmic design 
+					# loop,group,offset
+					Math
+						Sorting, Grouping, Iterative, Fractual, Noise
+					Performance optimization
+
+				Lighting concepts 
+				Shading concepts
+				Rendering concepts
+				Compositing concepts
+		def R&D_FX 
+			FX R&D(Practice) FX() + Flow() + Score() 
+			FX() = Basic_idea + Advance_development 
+			Flow() = Combiine_FX(Motion, movement, tension)
+			Score() = Look_dev() Innerbloom
