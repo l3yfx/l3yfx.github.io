90327fae8b9a8b540f3be093c659c471cff57241
l3yfx <75999309+l3yfx@users.noreply.github.com>
2021-01-03 22:30:20 +0800

    Update FXnote.py

--- FXnote.py
+++ FXnote.py
@@ -3,76 +3,102 @@ def l3yfx(self):
 	FX = 3D.Data_manipulation 
 	VFX = FX() + Composition() 
 	
-	# Essence, process, innerbloom 
+	-Essence, process, innerbloom 
 		Essence 
 			Nature inspired 
 		Process
 			Uterlize Computer science to create 3D visual.
 
-	# Technical
+	//Technical
 		def Foundation of 3D
-            Nature
-                Math/Physics
-            Computer science
-                3D Computer graphic/theory
-                    CG field
-                        Mathmatics/Physics
-                        algorithm
-                        Problem solving                        
-                    3D tech
-                        Modeling
-                        Dynamics
-                        Texturing
-                        Lighting
-                        Rendering
-                        Compositing
+            		Nature
+                		Math/Physics
+            		Computer science
+				3D Computer graphic/theory
+				    CG field
+					Mathmatics/Physics
+					algorithm
+					Problem solving                        
+				    3D tech
+					Modeling
+					Dynamics
+					Texturing
+					Lighting
+					Rendering
+					Compositing
 		def Realization of 3D 			
 			3D software/framwork/language 
 			CG industry Pipeline
 			Workflow(Using Houdini/Production efficieny)
 
-			FX concepts - Harness the power of houdini -> Realize nature/imagination in a efficient way.
-				3D data flow 
-					Data structure						
-						Point,vertex,primitive,mesh,normal,UVs,primuv
-						Volume 
-							Scalar field(density) 
-							Sign distance(SDF)
-							Vector field
-						PointCloud 
-					Data manipulation
-						Manipulate Reserved/custom attributes in 2 Dimensions
-							Proceduralism CRUD 3D data 
-							Simulation Dynamics 3D data					
-						Functions
-							Low_lev
-							High_lev
-					Data Organzation
-						Proceduralism
-					# Group,channel,ramp
-					# 3D FX Field/catergories
-					# 	Particles/Pyro/fluid/grains/crowds/finiteElementscloth/HairFur/vellum Oceans/wavesRipple
-					# 	destruction wires/rigidBody/ Growth/peeling/disintergration 
-					Tool 
-						Vop/Vex
-						AttributesTransfer 
-						solver
-						keyframe
-						Sidefx designed Node
-						Python
-						Expression
-				Algorithmic design 
-					# loop,group,offset
-					Math
-						Sorting, Grouping, Iterative, Fractual, Noise
-					Performance optimization
-
-				Lighting concepts 
-				Shading concepts
-				Rendering concepts
-				Compositing concepts
+                        3D FX concepts - Harness the power of houdini -> Realize nature/imagination in a efficient way.
+                            Data flow
+                                Data structure
+                                    Custom attributes
+                                    Point,vertex,primitive,mesh,normal,UVs
+                                    Volume
+                                        Scalar field(density)
+                                        Sign distance(SDF)
+                                        Vector field
+                                    PointCloud
+                                Data manipulation
+                                    Dimensions
+                                        Proceduralism CRUD 3D data
+                                        Simulation Dynamics 3D data 
+                                    Attributes
+                                        Reserved attributes                                        
+                                        Custom attributes
+                                        VEX/VOP processing
+                                    Functions
+                                        Operater
+                                            SOP, VOP, POP, DOP
+                                        Low-lev
+                                            AttributeTransfer, primuv, minpos....
+                                        High-lev
+                                            Particle, pyro, flip, vellum....
+                                        Custom fuction
+					    Sorting
+                                            Filtering
+                                    Networking
+					Organization
+					    Grouping
+					    Selection
+					    Pattern
+					    Expression
+					Referencing
+					    Channel/Parameter expression
+					Parameter expression
+					Data stream
+                                Data arrangement
+                                    Arrange data network into a procedural environment.
+                            Algorithmic design
+                                Constructing data flow for specific fx.
+                                    #Sorting, searching, filtering, Grouping data.
+				    Motion
+				    Flow
+				    Tension
+                                Implement mathmatical concepts/theory for R&D process. 
+                                    Iterative, Fractual
+                                Refine data flow process for performance optimaization                            
+                            Data Organzation
+                                Create digital asset for speeding up workflow.
+                                Create tool/plugin for optimize workflow.
+                                
+                        3D Lighting concepts
+                        3D Shading concepts
+                            Procedual shading
+                                Principled shader
+                                Displacement
+                                Micro surface
+                            Create/assign Material
+                            Apply map
+                                Texture
+                                Normal
+                                Bump
+                        3D Rendering concepts
+                        3D Compositing concepts
 		def R&D_FX 
 			FX R&D(Practice) FX() + Flow() + Score() 
 			FX() = Basic_idea + Advance_development 
 			Flow() = Combiine_FX(Motion, movement, tension)
-			Score() = Look_dev() Innerbloom 
+			Score() = Look_dev() Innerbloom
