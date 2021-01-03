37c68c6bbaee27389333ea83e4141ea987c77d27
ivanlin <ivanlin@MBP.local>
2021-01-03 22:35:25 +0800

    20210103

--- FXnote.py
+++ FXnote.py
@@ -30,73 +30,72 @@ def l3yfx(self):
 			3D software/framwork/language 
 			CG industry Pipeline
 			Workflow(Using Houdini/Production efficieny)
-
-                        3D FX concepts - Harness the power of houdini -> Realize nature/imagination in a efficient way.
-                            Data flow
-                                Data structure
-                                    Custom attributes
-                                    Point,vertex,primitive,mesh,normal,UVs
-                                    Volume
-                                        Scalar field(density)
-                                        Sign distance(SDF)
-                                        Vector field
-                                    PointCloud
-                                Data manipulation
-                                    Dimensions
-                                        Proceduralism CRUD 3D data
-                                        Simulation Dynamics 3D data 
-                                    Attributes
-                                        Reserved attributes                                        
-                                        Custom attributes
-                                        VEX/VOP processing
-                                    Functions
-                                        Operater
-                                            SOP, VOP, POP, DOP
-                                        Low-lev
-                                            AttributeTransfer, primuv, minpos....
-                                        High-lev
-                                            Particle, pyro, flip, vellum....
-                                        Custom fuction
-					    Sorting
-                                            Filtering
-                                    Networking
-					Organization
-					    Grouping
-					    Selection
-					    Pattern
-					    Expression
-					Referencing
-					    Channel/Parameter expression
-					Parameter expression
-					Data stream
-                                Data arrangement
-                                    Arrange data network into a procedural environment.
-                            Algorithmic design
-                                Constructing data flow for specific fx.
-                                    #Sorting, searching, filtering, Grouping data.
-				    Motion
-				    Flow
-				    Tension
-                                Implement mathmatical concepts/theory for R&D process. 
-                                    Iterative, Fractual
-                                Refine data flow process for performance optimaization                            
-                            Data Organzation
-                                Create digital asset for speeding up workflow.
-                                Create tool/plugin for optimize workflow.
-                                
-                        3D Lighting concepts
-                        3D Shading concepts
-                            Procedual shading
-                                Principled shader
-                                Displacement
-                                Micro surface
-                            Create/assign Material
-                            Apply map
-                                Texture
-                                Normal
-                                Bump
-                        3D Rendering concepts
-                        3D Compositing concepts
+            3D FX concepts - Harness the power of houdini -> Realize nature/imagination in a efficient way.
+                Data flow
+                    Data structure
+                        Custom attributes
+                        Point,vertex,primitive,mesh,normal,UVs
+                        Volume
+                            Scalar field(density)
+                            Sign distance(SDF)
+                            Vector field
+                        PointCloud
+                    Data manipulation
+                        Dimensions
+                            Proceduralism CRUD 3D data
+                            Simulation Dynamics 3D data 
+                        Attributes
+                            Reserved attributes                                        
+                            Custom attributes
+                            VEX/VOP processing
+                        Functions
+                            Operater
+                                SOP, VOP, POP, DOP
+                            Low-lev
+                                AttributeTransfer, primuv, minpos....
+                            High-lev
+                                Particle, pyro, flip, vellum....
+                            Custom fuction
+		    					Sorting
+                                Filtering
+                        Networking
+							Organization
+							    Grouping
+							    Selection
+							    Pattern
+							    Expression
+							Referencing
+							    Channel/Parameter expression
+							Parameter expression
+							Data stream
+                    	Data arrangement
+                        	Arrange data network into a procedural environment.
+                Algorithmic design
+                    Constructing data flow for specific fx.
+                        #Sorting, searching, filtering, Grouping data.
+					    Motion (Effect based)
+					    Flow (Time based)
+					    Tension (Layer based)
+                    Implement mathmatical concepts/theory for R&D process. 
+                        Iterative, Fractual
+                    Refine data flow process for performance optimaization                            
+                Data Organzation
+                    Create digital asset for speeding up workflow.
+                    Create tool/plugin for optimize workflow.
+                    
+            3D Lighting concepts
+            3D Shading concepts
+                Procedual shading
+                    Principled shader
+                    Displacement
+                    Micro surface
+                Create/assign Material
+                Apply map
+                    Texture
+                    Normal
+                    Bump
+            3D Rendering concepts
+            3D Compositing concepts
 		def R&D_FX 
 			FX R&D(Practice) FX() + Flow() + Score() 
 			FX() = Basic_idea + Advance_development 
