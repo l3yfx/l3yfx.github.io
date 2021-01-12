5e1c8a727e4e13abdd7d24ba858c1abf56677114
ivanlin <ivanlin@MBP.local>
2021-01-13 00:55:41 +0800

    20210112

--- FXnote.py
+++ FXnote.py
@@ -36,60 +36,65 @@ def l3yfx(self):
 				    CG industry
 				    Pipeline
 				    Workflow(Using Houdini/Production efficieny)
-					Expression
-					Wrangle
-					OP
-					Python
+						Expression
+						Wrangle
+						OP
+						Python
 				3D FX concepts - Harness the power of houdini -> Realize nature/imagination in a efficient way.
 				    Data flow
-					Data structure
-					    Custom attributes
-					    Point,vertex,primitive,mesh,normal,UVs
-					    Volume
-						Scalar field(density)
-						Sign distance(SDF)
-						Vector field
-					    PointCloud
-					Data manipulation
-					    Attributes
-						Data types
-						    float, int, array, vector, matrix, string
-						Reserved/Custom attributes       
-					    Functions
-						Operater
-						    SOP, VOP, POP, DOP
-						Low-lev
-						    AttributeTransfer, primuv, minpos, length....
-						High-lev
-						    Particle, pyro, flip, vellum....
-						Custom fuction
-						    Sorting
-						    Filtering
-					    Operation
-						Conditional
-						    Grouping
-							Selection
-							Pattern
-							Expression
-						Loop
-					Data stream
-					    Networking
-						Parameter/Channel Referencing
-						Parameter/Channel expression
-						Proceduralism
+						Data structure
+						    Point,vertex,primitive,mesh,normal,UVs
+						    Volume
+							Scalar field(density)
+							Sign distance(SDF)
+							Vector field
+						    PointCloud
+						Data manipulation
+						    Attributes
+						    	Context
+								Data types
+								    float, int, array, vector, matrix, string
+								Reserved attributes
+									Normal
+								Custom attributes       
+						    Functions
+								Operater
+								    SOP, VOP, POP, DOP
+								Low-lev
+								    AttributeTransfer, primuv, minpos, length....
+								High-lev
+								    Particle, pyro, flip, vellum....
+								Custom fuction
+								    Sorting
+								    Filtering
+						    Operation
+								Conditional
+								    Grouping
+									Selection
+									Pattern
+									Expression
+								Loop
+						Data stream
+						    Networking
+								Parameter/Channel Referencing
+								Parameter/Channel expression
+									Hscript expression
+									Global/Local variable
+									Geo attributes
+							Proceduralism
 				    Algorithmic design
-					Constructing data flow for specific fx.
-					    #Sorting, searching, filtering, Grouping data.
-					    Motion (Effect based)
-						Dimensions
-						    Proceduralism CRUD 3D data
-						    Simulation Dynamics 3D data 
-					    Flow (Time based)
-						Frame control
-					    Tension (Layer based)
-					Implement mathmatical concepts/theory for R&D process. 
-					    Iterative, Fractual
-					Refine data flow process for performance optimaization                            
+						Constructing data flow for specific fx.
+						    #Sorting, searching, filtering, Grouping data.
+						    Motion (Effect based)
+								Dimensions
+							   		Proceduralism CRUD 3D data
+							   		Simulation Dynamics 3D data 
+						    Flow (Time based)
+								Frame control
+						    Tension (Layer based)
+						Implement mathmatical concepts/theory for R&D process. 
+						    Iterative, Fractual
+						Refine data flow process for performance optimaization                            
 				    Data Organzation
 					Create digital asset for speeding up workflow.
 					Create tool/plugin for optimize workflow.
