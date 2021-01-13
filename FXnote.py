d3756e50246f85230ba87614d82d4b357b1b1430
l3yfx <75999309+l3yfx@users.noreply.github.com>
2021-01-13 18:59:57 +0800

    Update FXnote.py

--- FXnote.py
+++ FXnote.py
@@ -1,76 +1,110 @@
-def l3yfx(self):
-	3D = Modeling, animation, texture, lighting, rendering 	
-	FX = 3D.Data_manipulation 
-	VFX = FX() + Composition() 
-	
-	-Essence, process, innerbloom 
-		Essence 
-			Nature inspired 
-		Process
-			Uterlize Computer science to create 3D visual.
-
-	//Technical
-		def Foundation of 3D
-            		Nature
-                		Math/Physics
-            		Computer science
-				3D Computer graphic/theory
-				    CG field
-					Mathmatics/Physics
-					algorithm
-					Problem solving                        
-				    3D tech
-					Modeling
-					Dynamics
-					Texturing
-					Lighting
-					Rendering
-					Compositing
-		def Realization of 3D 			
-			3D software/framwork/language 
-			CG industry Pipeline
-			Workflow(Using Houdini/Production efficieny)
-
+    def l3yfx(self):
+        3D_Concepts = CG_knowledge, software, workflow        
+        3D = Modeling, animation, texture, lighting, rendering
+        3D_FX = 3D.Data_manipulation
+        
+        VFX = FX() + Composition()
+        
+        -Essence, process, innerbloom
+        
+        Essence
+            Nature inspired
+        Process
+        # Technical
+            Uterlize Computer science to create 3D visual.
+                Fundemental(Technical)
+                    def Foundation of 3D Computer graphic
+                        Nature
+                            Math/Physics
+                                Noise
+                                Motions
+                        Computer science
+                            CS
+                                Multithread process
+                                    VEX
+                            3D Computer graphic/theory
+                                CG field
+                                    Mathmatics/Physics
+                                    algorithm
+                                    Problem solving                     
+                                3D tech
+                                    Modeling
+                                    Dynamics
+                                    Texturing
+                                    Lighting
+                                    Rendering
+                                    Compositing         
+                    def Realization of 3D Computer graphic/theory
+                        3D software/framwork/language
+                            CG industry
+                            Pipeline
+                            Workflow(Using Houdini/Production efficieny)
+                                Expression
+                                Wrangle
+                                OP
+                                Python
                         3D FX concepts - Harness the power of houdini -> Realize nature/imagination in a efficient way.
                             Data flow
                                 Data structure
-                                    Custom attributes
-                                    Point,vertex,primitive,mesh,normal,UVs
-                                    Volume
-                                        Scalar field(density)
-                                        Sign distance(SDF)
-                                        Vector field
-                                    PointCloud
+                                    Data types
+                                        Low-lev
+                                            float, int, array, vector, matrix, string
+                                        High-lev
+                                            Geometry
+                                                Point,vertex,primitive,detail,Normal,UVs
+                                                Bezier/NURBS
+                                                Volume
+                                                    Scalar field(density)
+                                                    Sign distance(SDF)
+                                                    Vector field
+                                                PointCloud
                                 Data manipulation
-                                    Dimensions
-                                        Proceduralism CRUD 3D data
-                                        Simulation Dynamics 3D data 
                                     Attributes
-                                        Reserved attributes                                        
-                                        Custom attributes
-                                        VEX/VOP processing
+                                        Context
+                                            Point,vertex,primitive,detail                                            
+                                        Reserved/Custom attributes
+                                            Normal,UVs,Vel
                                     Functions
                                         Operater
                                             SOP, VOP, POP, DOP
                                         Low-lev
-                                            AttributeTransfer, primuv, minpos....
+                                            AttributeTransfer, primuv, minpos, length,....
                                         High-lev
                                             Particle, pyro, flip, vellum....
                                         Custom fuction
-                                    Organization
-                                        Grouping
-                                            Selection
-                                            Pattern
-                                            Expression
-                                        Referencing
-                                            Channel/Parameter expression
-                                        Sorting
-                                        Filtering
-                                Data arrangement
-                                    Arrange data network into a procedural environment.
+                                            Sorting
+                                            Filtering
+                                        Noise
+                                            Curlnoise, turbulentnoise, voronoise,....
+                                    Operation
+                                        Conditional
+                                            Grouping
+                                                Selection
+                                                Pattern
+                                                Expression
+                                        Loop
+                                        Copy/Instancing
+                                Data stream
+                                    Networking(Proceduralism)
+                                        Parameter/Channel Referencing
+                                        Parameter/Channel expression
+                                            Hscript expression
+                                            Global/Local variable
+                                            Geo attributes                                        
+                                        Compile blocks
+                                    File system
+                                        Cache to disk
+                                        
                             Algorithmic design
                                 Constructing data flow for specific fx.
-                                    Sorting, searching, filtering, Grouping data.
+                                    #Sorting, searching, filtering, Grouping data.
+                                    Motion (Effect based)
+                                        Dimensions
+                                            Proceduralism CRUD 3D data
+                                            Simulation Dynamics 3D data 
+                                    Flow (Time based)
+                                        Frame control
+                                    Tension (Layer based)
                                 Implement mathmatical concepts/theory for R&D process. 
                                     Iterative, Fractual
                                 Refine data flow process for performance optimaization                            
@@ -91,8 +125,3 @@ def l3yfx(self):
                                 Bump
                         3D Rendering concepts
                         3D Compositing concepts
-		def R&D_FX 
-			FX R&D(Practice) FX() + Flow() + Score() 
-			FX() = Basic_idea + Advance_development 
-			Flow() = Combiine_FX(Motion, movement, tension)
-			Score() = Look_dev() Innerbloom
