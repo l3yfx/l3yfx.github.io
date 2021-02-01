5d8267dc9ff5ccb2426497109953a121108c40bc
l3yfx <75999309+l3yfx@users.noreply.github.com>
2021-02-01 18:44:10 +0800

    Create FX_note.py

---
+++ FX_note.py
@@ -0,0 +1,15 @@
+class FX R&D
+	def Growth
+    # 1 way transform.
+        Copy&Instance #Project_Crassula
+        Noise + @P transform #Project_SnakeIsland
+        Move & trail #Project_NikeFlyknit
+        VDB modeling #Project_Organic
+    def Morph
+    # 2 ways transform.          
+        UV xform #Project_LineMorph #Project_Venus
+        @P mix function #Project_Knitting
+    def Magic
+        Vector field Vel #Project_ParticleFluid
+        POP force #Project_Cradence
+        
